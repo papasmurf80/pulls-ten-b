a935397eeca03ea1a99efd5ef01fd823329ff78a
Joseph (Joey) Atwell <joeyatwell@gmail.com>
2019-05-09 13:18:43 -0700

    custom npm command 'pulls-ten' created. Runs app.js file

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 const request = require('request');
 const fileName = require('fs');
 
-let tenSites = ["www.google.com", "www.yahoo.com"];
+let tenSites = ["www.google.com", "www.yahoo.com", "www.yamahamotorsports.com"];
 // Loop through array of sites. For each iteration, create a new file/name(createWriteStream destination). 
 
-function testRun(){
-    console.log("test run")
-};
 
-testRun(); 
-
-// request.get("http://www.google.com", function (error, response,body){
-//     console.log("request ran")
-//     console.error("error:", error);
-//     console.log("body:", body)
-// });
 
 for (let i = 0; i < tenSites.length; i ++){
-    request.get("http://"+tenSites[i], function(error, response, body){
-        fileName.createWriteStream("file" + i + ".html");
-        console.error('error:', error); //Prints error
-        console.log(i + "this works");
-        //console.log("body:", body); //prints HTML to console
+    request.get("http://"+tenSites[i], function(error, response, body){ //The request - loops through array of websites
+        fileName.createWriteStream("file" + i + ".html");//Writes files to subfolder
+        console.error('error:', error); //Prints error if one is thrown
+        console.log(i + "this works");//Provides reassurance
     });
 };
-
-
-// The request 
-
-
-
-// --------------
-
-// Moves files to subfolder 
-// --------------
 
 // Custom npm command
 // --------------
