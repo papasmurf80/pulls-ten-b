f2a8d85f99ce6fee6c0ce6489335781a856a6f9d
Joseph (Joey) Atwell <joeyatwell@gmail.com>
2019-05-08 20:55:49 -0700

    request and get functioning properly, returns html body of requested site

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const request = require('request');
 const fileName = require('fs');
 
-let tenSites = ["www.google.com", "www.yahooo.com"];
+let tenSites = ["www.google.com", "www.yahoo.com"];
 // Loop through array of sites. For each iteration, create a new file/name(createWriteStream destination). 
 
-request.get("http://www.google.com", function (error, response,body){
-    console.error("error:", error);
-    console.log(i + "request successful")
-})
+function testRun(){
+    console.log("test run")
+};
 
+testRun(); 
+
+request.get("http://www.google.com", function (error, response,body){
+    console.log("request ran")
+    console.error("error:", error);
+    console.log("body:", body)
+});
 // for (let i = 0; i < tenSites.length; i ++){
 //     request.get(tenSites[i], function(error, response, body){
 //         fileName.createWriteStream("file" + i + ".html");
