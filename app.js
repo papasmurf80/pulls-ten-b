ddab9f1ef6e6cf07ccf14ee231d7f42e6d7acdb7
Joseph (Joey) Atwell <joeyatwell@gmail.com>
2019-05-10 13:02:43 -0700

    created input interface, import FormsModule for binding

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+let tenSites = [{sitename}];
 
-const request = require('request');
-const fileName = require('fs');
+var scraper = function(){
+    const request = require('request');
+    const fileName = require('fs');
 
-let tenSites = ["www.google.com", "www.yahoo.com", "www.yamahamotorsports.com"];
+    
 // Loop through array of sites. For each iteration, create a new file/name(createWriteStream destination). 
 
 
 
-for (let i = 0; i < tenSites.length; i ++){
-    request.get("http://"+tenSites[i], function(error, response, body){ //The request - loops through array of websites
-        fileName.createWriteStream("file" + i + ".html");//Writes files to subfolder
-        console.error('error:', error); //Prints error if one is thrown
-        console.log(i + "this works");//Provides reassurance
-    });
+    for (let i = 0; i < tenSites.length; i ++){
+        request.get("http://"+tenSites[i], function(error, response, body){ //The request - loops through array of websites
+            fileName.createWriteStream("file" + i + ".html");//Writes files to subfolder
+            console.error('error:', error); //Prints error if one is thrown
+            console.log(i + "this works");//Provides reassurance
+        });
+    };
 };
 
+module.exports={
+    scraper: scraper,
+    tenSites: tenSites
+}
